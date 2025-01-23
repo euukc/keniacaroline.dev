94427e012314be3eb1929740b0969cb1bc93167d
euukc <keniacarolineferreira@gmail.com>
2025-01-23 10:46:57 -0300

    Coloquei 2 projetos com status de em desenvolvimento

--- src/app/projetos/page.jsx
+++ src/app/projetos/page.jsx
@@ -84,14 +84,12 @@ export default function Projetos() {
                                 </span>
                             </div>
                             <div className='mt-4 flex flex-row gap-2 flex-wrap pb-6'>
-                                <Tooltip
-                                    className='text-red-400'
-                                    content="Em desenvolvimento, volte em breve!" showArrow={true} placement="right">
-                                    <p className="gap-2 inline-flex items-center justify-center whitespace-nowrap rounded-md font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 border border-input bg-background hover:bg-accent  h-10 px-4 py-2 text-sm cursor-not-allowed" href="#" target='_blank'>
-                                        <i className="bi bi-arrow-up-right"></i>
-                                        Visitar
-                                    </p>
-                                </Tooltip>
+
+                                <p className="gap-2 inline-flex items-center justify-center whitespace-nowrap rounded-md font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 border border-input bg-background hover:bg-accent  h-10 px-4 py-2 text-sm cursor-not-allowed" href="#" target='_blank'>
+                                    {/* <i className="bi bi-arrow-up-right"></i> */}
+                                    Em desenvolvimento
+                                </p>
+
                             </div>
                         </div>
                     </div>
@@ -283,14 +281,11 @@ export default function Projetos() {
                             </div>
                             <div className='mt-4 flex flex-row gap-2 flex-wrap'>
 
-                                <Tooltip
-                                    className='text-red-400'
-                                    content="Em desenvolvimento, volte em breve!" showArrow={true} placement="right">
-                                    <p className="gap-2 inline-flex items-center justify-center whitespace-nowrap rounded-md font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 border border-input bg-background hover:bg-accent  h-10 px-4 py-2 text-sm cursor-not-allowed" href="#" target='_blank'>
-                                        <i className="bi bi-arrow-up-right"></i>
-                                        Visitar
-                                    </p>
-                                </Tooltip>
+                                <p className="gap-2 inline-flex items-center justify-center whitespace-nowrap rounded-md font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 border border-input bg-background hover:bg-accent  h-10 px-4 py-2 text-sm cursor-not-allowed" href="#" target='_blank'>
+                                    {/* <i className="bi bi-arrow-up-right"></i> */}
+                                    Em desenvolvimento
+                                </p>
+
                             </div>
                         </div>
                     </div>
